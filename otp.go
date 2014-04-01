f81c9c656d87b5921dc61b3033977275951c8c9b
Ryan Uber <ru@ryanuber.com>
2014-03-31 22:13:31 -0700

    Added some comments to the source code

--- otp.go
+++ otp.go
@@ -9,11 +9,13 @@ type Pad struct {
 	currentPage int
 }
 
+// NewPad creates a new "one-time pad"
 func NewPad(material []byte, pageSize int, startPage int) (*Pad, error) {
 	if len(material)%pageSize != 0 {
 		return nil, fmt.Errorf("pad size must be divisible by page size")
 	}
 
+	// Do the page-splitting work up front
 	var pages [][]byte
 	for i := 0; i < len(material); i += pageSize {
 		pages = append(pages, material[i:i+pageSize])
@@ -31,18 +33,22 @@ func NewPad(material []byte, pageSize int, startPage int) (*Pad, error) {
 	return &p, nil
 }
 
+// TotalPages returns the number of pages in the pad
 func (p *Pad) TotalPages() int {
 	return len(p.pages)
 }
 
+// UnusedPages returns the number of unused pages in the pad
 func (p *Pad) RemainingPages() int {
 	return len(p.pages) - (p.currentPage + 1)
 }
 
+// UsedPages returns the number of pages that have been used
 func (p *Pad) UsedPages() int {
 	return p.currentPage + 1
 }
 
+// PreviousPage returns the payload of the last used page
 func (p *Pad) PreviousPage() ([]byte, error) {
 	if p.currentPage == 0 {
 		return nil, fmt.Errorf("no previous pages")
@@ -50,10 +56,13 @@ func (p *Pad) PreviousPage() ([]byte, error) {
 	return p.pages[p.currentPage-1], nil
 }
 
+// CurrentPage returns the payload of the current page
 func (p *Pad) CurrentPage() []byte {
 	return p.pages[p.currentPage]
 }
 
+// NextPage will advance the page pointer, and return the payload of the
+// new current key.
 func (p *Pad) NextPage() ([]byte, error) {
 	if p.RemainingPages() == 0 {
 		return nil, fmt.Errorf("pad depleted")
@@ -62,6 +71,8 @@ func (p *Pad) NextPage() ([]byte, error) {
 	return p.CurrentPage(), nil
 }
 
+// PeekNextPage returns the payload of the next page without advancing the
+// page pointer.
 func (p *Pad) PeekNextPage() ([]byte, error) {
 	if p.RemainingPages() == 0 {
 		return nil, fmt.Errorf("no pages left")
